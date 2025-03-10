09e70469af26a64d2d7c8d90b8ac5776ecdbb0cd
seondays <agseon9@gmail.com>
2025-03-10 20:03:38 +0900

    Initial commit

---
+++ src/main/java/seondays/shareticon/ShareticonApplication.java
@@ -0,0 +1,13 @@
+package seondays.shareticon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareticonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareticonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seondays/shareticon/ShareticonApplicationTests.java
@@ -0,0 +1,13 @@
+package seondays.shareticon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareticonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
