a4d5b5b02b60776baa998aec5daa5440b417e698
seondays <agseon9@gmail.com>
2025-03-13 20:13:59 +0900

    feat : 빌더패턴을 위한 어노테이션 추가 #2

--- src/main/java/seondays/shareticon/voucher/Voucher.java
+++ src/main/java/seondays/shareticon/voucher/Voucher.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,7 +21,9 @@ import seondays.shareticon.user.User;
 @Table(name = "voucher")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Voucher {
 
     @Id
