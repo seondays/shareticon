dcc64b0c0ae963367f7c5a7f4a065274f40a0e5e
seondays <agseon9@gmail.com>
2025-03-24 18:21:30 +0900

    feat : octet stream 전용 역직렬화 컨버터 구현 #6
    swagger 에서 쿠폰 등록 api 테스트 시 octet stream 문제 해결을 위한 커스텀 컨버터 작성

---
+++ src/main/java/seondays/shareticon/utils/OctetStreamCustomConverter.java
@@ -0,0 +1,30 @@
+package seondays.shareticon.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.reflect.Type;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OctetStreamCustomConverter extends AbstractJackson2HttpMessageConverter {
+
+    public OctetStreamCustomConverter(ObjectMapper objectMapper) {
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        return false;
+    }
+}
