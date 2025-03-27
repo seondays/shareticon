3fbc93cd22aae3d1c49d2030f1f2b4a1181f6cea
seondays <agseon9@gmail.com>
2025-03-27 23:50:26 +0900

    refactor : 쿠폰 이미지 관련 예외 클래스명 수정 #6

--- src/main/java/seondays/shareticon/exception/NoVoucherImageException.java
+++
@@ -1,8 +0,0 @@
-package seondays.shareticon.exception;
-
-public class NoVoucherImageException extends RuntimeException {
-
-    public NoVoucherImageException() {
-        super("쿠폰 이미지가 포함되어야 합니다");
-    }
-}
