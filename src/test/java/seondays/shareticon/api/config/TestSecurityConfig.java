c45e6df69eb2a84e5f5ba2c334ec0def49fdcb7f
seondays <agseon9@gmail.com>
2025-05-02 00:01:29 +0900

    test : 로그아웃 컨트롤러 테스트 코드 작성 #6

--- src/test/java/seondays/shareticon/api/config/TestSecurityConfig.java
+++ src/test/java/seondays/shareticon/api/config/TestSecurityConfig.java
@@ -1,9 +1,11 @@
 package seondays.shareticon.api.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -17,6 +19,15 @@ public class TestSecurityConfig {
                                 "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**")
                         .permitAll()
                         .anyRequest().authenticated());
+
+        http.logout(LogoutConfigurer::disable);
+
+        // 로그인하지 않은 요청을 가정하는 경우, 익명 객체가 사용되므로 이 때도 401을 반환해주도록 상황을 설정하여 프로덕션 코드 동작과 일치시킴
+        http.exceptionHandling(exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                })
+        );
 
         return http.build();
     }
