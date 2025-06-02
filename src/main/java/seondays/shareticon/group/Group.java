7562c524b68659ca01281ffe0dd800dd109dd6d6
seondays <agseon9@gmail.com>
2025-06-02 20:03:26 +0900

    feat : 그룹 엔티티가 조회를 위한 그룹 유저 정보를 가지도록 변경 #23

--- src/main/java/seondays/shareticon/group/Group.java
+++ src/main/java/seondays/shareticon/group/Group.java
@@ -8,12 +8,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import seondays.shareticon.userGroup.UserGroup;
 import seondays.shareticon.utils.BaseEntity;
 import seondays.shareticon.user.User;
 
@@ -34,4 +38,6 @@ public class Group extends BaseEntity {
     @Column(unique = true)
     private String inviteCode;
     private String title;
+    @OneToMany(mappedBy = "group", fetch = FetchType.LAZY)
+    private List<UserGroup> userGroups = new ArrayList<>();
 }
