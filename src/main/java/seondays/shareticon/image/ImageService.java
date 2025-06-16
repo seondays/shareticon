6bab0ece4aab334f6cf1404a7785257b9abdc7bb
seondays <agseon9@gmail.com>
2025-06-16 20:59:01 +0900

    feat : presigned URL 생성 기능 구현 #25

--- src/main/java/seondays/shareticon/image/ImageService.java
+++ src/main/java/seondays/shareticon/image/ImageService.java
@@ -1,26 +1,29 @@
 package seondays.shareticon.image;
 
 import java.io.IOException;
-import java.net.URL;
+import java.time.Duration;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import seondays.shareticon.exception.ImageUploadException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 @Service
+@RequiredArgsConstructor
 public class ImageService {
 
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
     @Value("${aws.s3.bucket}")
     private String bucket;
-
-    public ImageService(S3Client s3Client) {
-        this.s3Client = s3Client;
-    }
 
     public String uploadImage(MultipartFile image) {
         String uploadTitle = makeUploadTitle("voucher", image.getOriginalFilename());
@@ -37,12 +40,7 @@ public class ImageService {
 
             s3Client.putObject(putObjectRequest, requestBody);
 
-            URL url = s3Client.utilities().getUrl(builder -> builder
-                    .bucket(bucket)
-                    .key(uploadTitle)
-                    .build());
-
-            return url.toString();
+            return uploadTitle;
         } catch (IOException e) {
             throw new ImageUploadException();
         }
@@ -50,5 +48,25 @@ public class ImageService {
 
     public String makeUploadTitle(String prefix, String filename) {
         return prefix + "/" + UUID.randomUUID() + "-" + filename;
+    }
+
+    public String getPreSignedImageUrl(String objectKey, long expirationMinutes) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(objectKey)
+                .build();
+
+        GetObjectPresignRequest presignedUrlGenerationRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(expirationMinutes))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        try {
+            PresignedGetObjectRequest generatedPresignedUrl = s3Presigner.presignGetObject(
+                    presignedUrlGenerationRequest);
+            return generatedPresignedUrl.url().toString();
+        } catch (Exception e) {
+            throw new ImageUploadException();
+        }
     }
 }
