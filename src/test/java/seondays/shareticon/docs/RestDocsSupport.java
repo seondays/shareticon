1d25abff06cb67fba99c62710c6f1ef793efdfe2
seondays <agseon9@gmail.com>
2025-06-17 16:15:08 +0900

    test : 쿠폰 생성 request 만료일 검증 로직 문서화를 위한 서포트 클래스 커스텀 validator 추가 #24

--- src/test/java/seondays/shareticon/docs/RestDocsSupport.java
+++ src/test/java/seondays/shareticon/docs/RestDocsSupport.java
@@ -5,6 +5,12 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.validation.Validation;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.validation.Validator;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -16,6 +22,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 import seondays.shareticon.login.CustomOAuth2User;
 import seondays.shareticon.user.dto.UserOAuth2Dto;
 
@@ -34,10 +41,12 @@ public abstract class RestDocsSupport {
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
         // setMessageConverters를 통해 mockMvc의 LocalDate 역직렬화를 위한 컨버터 추가
+        // DTO 검증을 수행할 validator에서 우리가 지정한 clock 객체를 사용하도록 직접 설정
         this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
                 .apply(documentationConfiguration(provider))
                 .setCustomArgumentResolvers(new AuthenticationPrincipalArgumentResolver())
                 .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+                .setValidator(validator(clock()))
                 .build();
 
         mockUser = new CustomOAuth2User(new UserOAuth2Dto(1L, "user", "ROLE_USER"));
@@ -47,4 +56,17 @@ public abstract class RestDocsSupport {
     }
 
     protected abstract Object initController();
+
+
+    private Validator validator(Clock clock) {
+        ValidatorFactory factory = Validation.byDefaultProvider()
+                .configure()
+                .clockProvider(() -> clock)
+                .buildValidatorFactory();
+        return new SpringValidatorAdapter(factory.getValidator());
+    }
+
+    private Clock clock() {
+        return Clock.fixed(Instant.parse("2024-12-01T00:00:00Z"), ZoneId.of("Asia/Seoul"));
+    }
 }
