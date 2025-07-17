d313438a11be888cae43edaa114df5c7b5bbaebe
seondays <agseon9@gmail.com>
2025-07-17 17:16:59 +0900

    fix : 유저가 가입한 그룹의 수 조회 시, 오로지 JOINED으로 정상 가입된 경우만 카운팅하도록 수정 #40
    유저가 가입한 그룹을 정상적으로 카운팅하도록 로직 수정

--- src/main/java/seondays/shareticon/userGroup/UserGroupRepository.java
+++ src/main/java/seondays/shareticon/userGroup/UserGroupRepository.java
@@ -13,16 +13,20 @@ public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
 
     boolean existsByUserIdAndGroupId(Long userId, Long groupId);
 
-    Long countByUserId(Long userId);
+    @Query("""
+            SELECT COUNT(ug)
+            FROM UserGroup ug
+            WHERE ug.user.id = :userId AND ug.joinStatus = 'JOINED'""")
+    Long countByUserIdAndJoined(Long userId);
 
     Optional<UserGroup> findByUserIdAndGroupId(Long userId, Long groupId);
 
     @Query("""
-            select ug
-              from UserGroup ug
+            SELECT ug
+              FROM UserGroup ug
               JOIN FETCH ug.user
-              where ug.group.leaderUser.id = :leaderId
-                and ug.joinStatus = :status
+              WHERE ug.group.leaderUser.id = :leaderId
+                AND ug.joinStatus = :status
                 """)
     List<UserGroup> findByLeaderAndJoinStatus(Long leaderId, JoinStatus status);
 
@@ -39,5 +43,6 @@ public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
             AND ug.joinStatus in :status
             GROUP BY g.id, ug.groupTitleAlias
             """)
-    List<GroupListResponse> findGroupsWithMemberCountByUserIdAndStatus(Long userId, List<JoinStatus> status);
+    List<GroupListResponse> findGroupsWithMemberCountByUserIdAndStatus(Long userId,
+            List<JoinStatus> status);
 }
