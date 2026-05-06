1227e7fbd7df01df1c512e2619dcf0dee4806277
seondays <agseon9@gmail.com>
2026-05-06 20:04:18 +0900

    fix : 테스트 진행 시, RedisTestContainer를 사용하지 않던 문제 해결 #61

--- src/test/java/seondays/shareticon/api/config/IntegrationTestSupport.java
+++ src/test/java/seondays/shareticon/api/config/IntegrationTestSupport.java
@@ -2,33 +2,18 @@ package seondays.shareticon.api.config;
 
 import java.time.Clock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import seondays.shareticon.group.RandomCodeFactory;
-import seondays.shareticon.image.ImageService;
 
 @EnableScheduling
 @EnableRetry
-@Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest
-public abstract class IntegrationTestSupport {
-
-    private static final String REDIS_IMAGE = "redis:7.4.1-alpine";
-    private static final int REDIS_PORT = 6379;
-
-    @ServiceConnection(name = "redis")
-    private static final GenericContainer<?> REDIS_CONTAINER =
-            new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
-                    .withExposedPorts(REDIS_PORT)
-                    .withReuse(true);
+public abstract class IntegrationTestSupport extends RedisTestContainer {
 
     @MockitoSpyBean
      protected RandomCodeFactory randomCodeFactory;
